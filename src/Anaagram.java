9262abff18767677928ef7d63628549e381ad857
palugunakadupukotla <palugunak@gmail.com>
2022-08-08 17:36:52 -0400

    firstrepeatedhchar

---
+++ src/Anaagram.java
@@ -0,0 +1,14 @@
+public class Anaagram {
+
+    public String anagram(String S1, String S2) {
+        // anagram mean both the string should be matched
+
+        if(S1.length() < S2.length()){
+         return  S1;
+        }
+
+        return S2;
+
+
+    }
+}
